d0e9dd5423c82d439fe0e5521ce937b638360997
OmarHansali <hansaliomar18@gmail.com>
2024-06-18 02:30:41 +0100

    admin creation

--- client/src/client/pages/Reservation/ReserveCategory.tsx
+++ client/src/client/pages/Reservation/ReserveCategory.tsx
@@ -3,6 +3,30 @@ import ProgramSection from "../../component/campus/ProgramSection2";
 import Layout7 from "../../component/layout/Layout7";
 
 
+  // create admin account
+  const admin = {
+    username: "admin",
+    password: "admin",
+    role: "admin",
+    email: "??@gmail.com",
+    phoneNumber: "1234567890"
+  }
+  try {
+    const response = await fetch('http://localhost:8080/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(admin),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+  } catch (error) {
+    console.error('Error during creating account:', error);
+  }
+
 const Reservation = () => {
   return (
 
